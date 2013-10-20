6ac9bf01e34c1219199d911487d465097dc89c9e
Marcus Hoffmann <bubuiic@aol.com>
2013-10-20 02:20:18 +0200

    further skeleton

--- fchodb.py
+++ fchodb.py
@@ -1,4 +1,6 @@
 import pymysql
+import smtplib
+
 class memberEntry:
     def __init__(self, data):
         self.number = data[0]
@@ -9,6 +11,14 @@ class memberEntry:
         self.homepage = data[10]
         self.phones = (data[11],data[12],data[13])
         self.adresses = ((data[14],data[15],data[16],data[17],data[18],data[19]),(data[20],data[21],data[22],data[23],data[24],data[25]),(data[26],data[27],data[28],data[29],data[30],data[31]))
+
+    def outputPdf(self):
+        pass
+
+    def outputHtml(self):
+        pass
+    
+
 
 if __name__ == "__main__":
     print('Connecting to DB')
